5a037460dc8671d4ed4494bb9943fbfeb3f0f608
Afifa Khalid <afifkhal529@ed.amdsb.ca>
2022-06-07 13:35:35 -0400

    Add project files.

---
+++ Brick Breaker/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Brick_Breaker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.subtitleLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeLabel.Location = new System.Drawing.Point(0, 9);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(64, 19);
+            this.timeLabel.TabIndex = 0;
+            this.timeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.scoreLabel.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreLabel.Location = new System.Drawing.Point(508, 533);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(64, 19);
+            this.scoreLabel.TabIndex = 1;
+            this.scoreLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.titleLabel.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(147, 212);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(238, 19);
+            this.titleLabel.TabIndex = 2;
+            this.titleLabel.Text = "title";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // subtitleLabel
+            // 
+            this.subtitleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.subtitleLabel.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subtitleLabel.Location = new System.Drawing.Point(92, 240);
+            this.subtitleLabel.Name = "subtitleLabel";
+            this.subtitleLabel.Size = new System.Drawing.Size(345, 19);
+            this.subtitleLabel.TabIndex = 3;
+            this.subtitleLabel.Text = "subtitle";
+            this.subtitleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.subtitleLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.scoreLabel);
+            this.Controls.Add(this.timeLabel);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label subtitleLabel;
+    }
+}
+
---
+++ Brick Breaker/Form1.cs
@@ -0,0 +1,349 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Brick_Breaker
+{
+    public partial class Form1 : Form
+    {
+        Rectangle hero = new Rectangle(280, 450, 60, 8);
+        Rectangle ball = new Rectangle(280, 300, 10, 10);
+
+        int herospeed = 15;
+        int ballXspeed = 0;
+        int ballYspeed = 8;
+
+        List<Rectangle> brickRow1 = new List<Rectangle>();
+        List<Rectangle> brickRow2 = new List<Rectangle>();
+        List<Rectangle> brickRow3 = new List<Rectangle>();
+        List<Rectangle> brickRow4 = new List<Rectangle>();
+        List<Rectangle> brickRow5 = new List<Rectangle>();
+        List<Rectangle> brickRow6 = new List<Rectangle>();
+
+        int score = 0;
+        int time = 0;
+
+        bool leftKey;
+        bool rightKey;
+
+        string gameState = "waiting";
+
+        SolidBrush whiteBrush = new SolidBrush(Color.Honeydew);
+        SolidBrush redBrush = new SolidBrush(Color.LightCoral);
+        SolidBrush orangeBrush = new SolidBrush(Color.Coral);
+        SolidBrush yellowBrush = new SolidBrush(Color.Khaki);
+        SolidBrush greenBrush = new SolidBrush(Color.DarkSeaGreen);
+        SolidBrush blueBrush = new SolidBrush(Color.MediumTurquoise);
+        SolidBrush purpleBrush = new SolidBrush(Color.Thistle);
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    leftKey = true;
+                    break;
+                case Keys.Right:
+                    rightKey = true;
+                    break;
+                case Keys.Space:
+
+                    if (gameState == "waiting" || gameState == "over")
+
+                    {
+                        GameInitialize();
+                    }
+                    break;
+                case Keys.Escape:
+
+                    if (gameState == "waiting" || gameState == "over")
+
+                    {
+                        Application.Exit();
+                    }
+                    break;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    leftKey = false;
+                    break;
+                case Keys.Right:
+                    rightKey = false;
+                    break;
+            }
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            //move ball 
+            ball.X += ballXspeed;
+            ball.Y += ballYspeed;
+
+            //move hero
+            if (leftKey == true && hero.X > 0)
+            {
+                hero.X -= herospeed;
+            }
+
+            if (rightKey == true && hero.X < this.Width - hero.Width)
+            {
+                hero.X += herospeed;
+            }
+
+            //ball intersection with side walls 
+            if (ball.X > this.Width || ball.X < 0)
+            {
+                ballXspeed *= -1;
+            }
+
+            //ball intersection with top wall
+            if (ball.Y < 0)
+            {
+                ballYspeed = -ballYspeed;
+            }
+
+            //ball intersection with player
+            if (hero.IntersectsWith(ball))
+            {
+                if (ballXspeed == 0)
+                {
+                    ballXspeed = 2;
+                    // give it an x speed  
+                }
+                ballYspeed *= -1;
+                ball.Y = hero.Y - ball.Height;
+            }
+
+            //check if player missed ball  
+            if (ball.Y > this.Height)
+            {
+                ball.X = 280;
+                ball.Y = 300;
+
+                hero.X = 280;
+                hero.Y = 450;
+            }
+
+            //check for point scored
+            for (int i = 0; i < brickRow1.Count(); i++)
+            {
+                if (ball.IntersectsWith(brickRow1[i]))
+                {
+                    score++;
+                    scoreLabel.Text = $"{score}";
+
+                    brickRow1.RemoveAt(i);
+                    ballXspeed *= -1;
+
+                    ball.X = 280;
+                    ball.Y = 300;
+
+                    hero.Y = 450;
+                }
+
+            }
+            for (int i = 0; i < brickRow2.Count(); i++)
+            {
+                if (ball.IntersectsWith(brickRow2[i]))
+                {
+                    score++;
+                    scoreLabel.Text = $"{score}";
+
+                    brickRow2.RemoveAt(i);
+                    ballXspeed *= -1;
+
+                    ball.X = 280;
+                    ball.Y = 300;
+
+                    hero.Y = 450;
+                }
+            }
+            for (int i = 0; i < brickRow3.Count(); i++)
+            {
+                if (ball.IntersectsWith(brickRow3[i]))
+                {
+                    score++;
+                    scoreLabel.Text = $"{score}";
+
+                    brickRow3.RemoveAt(i);
+                    ballXspeed *= -1;
+
+                    ball.X = 280;
+                    ball.Y = 300;
+
+                    hero.Y = 450;
+                }
+            }
+            for (int i = 0; i < brickRow4.Count(); i++)
+            {
+                if (ball.IntersectsWith(brickRow4[i]))
+                {
+                    score++;
+                    scoreLabel.Text = $"{score}";
+
+                    brickRow4.RemoveAt(i);
+                    ballXspeed *= -1;
+
+                    ball.X = 280;
+                    ball.Y = 300;
+
+                    hero.Y = 450;
+                }
+            }
+            for (int i = 0; i < brickRow5.Count(); i++)
+            {
+                if (ball.IntersectsWith(brickRow5[i]))
+                {
+                    score++;
+                    scoreLabel.Text = $"{score}";
+
+                    brickRow5.RemoveAt(i);
+                    ballXspeed *= -1;
+
+                    ball.X = 280;
+                    ball.Y = 300;
+
+                    hero.Y = 450;
+                }
+            }
+            for (int i = 0; i < brickRow6.Count(); i++)
+            {
+                if (ball.IntersectsWith(brickRow6[i]))
+                {
+                    score++;
+                    scoreLabel.Text = $"{score}";
+
+                    brickRow6.RemoveAt(i);
+                    ballXspeed *= -1;
+
+                    ball.X = 280;
+                    ball.Y = 300;
+
+                    hero.Y = 450;
+                }
+            }
+            Refresh();
+        }
+
+        public void GameInitialize()
+        {
+            titleLabel.Text = "";
+            subtitleLabel.Text = "";
+
+            score = 0;
+            scoreLabel.Text = "0";
+
+            gameTimer.Enabled = true;
+            gameState = "running";
+
+            brickRow1.Clear();
+            brickRow1.Add(new Rectangle(270, 270, 30, 10));
+
+            brickRow2.Clear();
+            brickRow2.Add(new Rectangle(250, 250, 30, 10));
+            brickRow2.Add(new Rectangle(290, 250, 30, 10));
+
+            brickRow3.Clear();
+            brickRow3.Add(new Rectangle(230, 230, 30, 10));
+            brickRow3.Add(new Rectangle(270, 230, 30, 10));
+            brickRow3.Add(new Rectangle(310, 230, 30, 10));
+
+            brickRow4.Clear();
+            brickRow4.Add(new Rectangle(210, 210, 30, 10));
+            brickRow4.Add(new Rectangle(250, 210, 30, 10));
+            brickRow4.Add(new Rectangle(290, 210, 30, 10));
+            brickRow4.Add(new Rectangle(330, 210, 30, 10));
+
+            brickRow5.Clear();
+            brickRow5.Add(new Rectangle(190, 190, 30, 10));
+            brickRow5.Add(new Rectangle(230, 190, 30, 10));
+            brickRow5.Add(new Rectangle(270, 190, 30, 10));
+            brickRow5.Add(new Rectangle(310, 190, 30, 10));
+            brickRow5.Add(new Rectangle(350, 190, 30, 10));
+
+            brickRow6.Clear();
+            brickRow6.Add(new Rectangle(170, 170, 30, 10));
+            brickRow6.Add(new Rectangle(210, 170, 30, 10));
+            brickRow6.Add(new Rectangle(250, 170, 30, 10));
+            brickRow6.Add(new Rectangle(290, 170, 30, 10));
+            brickRow6.Add(new Rectangle(330, 170, 30, 10));
+            brickRow6.Add(new Rectangle(370, 170, 30, 10));
+
+            hero.Y = this.Height - hero.Height - 50;
+            hero.X = this.Width - hero.Width - 280;
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (gameState == "waiting")
+            {
+                titleLabel.Text = "BRICK BREAKER";
+                subtitleLabel.Text = "Press Space Bar to Start or Escape to Exit";
+            }
+            else if (gameState == "running")
+            {
+
+                //draw hero 
+                e.Graphics.FillRectangle(whiteBrush, hero);
+
+                //draw ball
+                e.Graphics.FillRectangle(whiteBrush, ball);
+
+                //paint bricks
+                for (int i = 0; i < brickRow1.Count(); i++)
+                {
+                    e.Graphics.FillRectangle(redBrush, brickRow1[i]);
+                }
+                for (int i = 0; i < brickRow2.Count(); i++)
+                {
+                    e.Graphics.FillRectangle(orangeBrush, brickRow2[i]);
+                }
+                for (int i = 0; i < brickRow3.Count(); i++)
+                {
+                    e.Graphics.FillRectangle(yellowBrush, brickRow3[i]);
+                }
+                for (int i = 0; i < brickRow4.Count(); i++)
+                {
+                    e.Graphics.FillRectangle(greenBrush, brickRow4[i]);
+                }
+                for (int i = 0; i < brickRow5.Count(); i++)
+                {
+                    e.Graphics.FillRectangle(blueBrush, brickRow5[i]);
+                }
+                for (int i = 0; i < brickRow6.Count(); i++)
+                {
+                    e.Graphics.FillRectangle(purpleBrush, brickRow6[i]);
+                }
+            }
+
+            else if (gameState == "over")
+            {
+                scoreLabel.Text = $"Score: {score}";
+
+                titleLabel.Text = "GAME OVER";
+                subtitleLabel.Text = $"Your final score was {score}";
+
+                subtitleLabel.Text += "\nPress Space Bar to Start or Escape to Exit";
+
+            }
+
+        }
+    }
+}
